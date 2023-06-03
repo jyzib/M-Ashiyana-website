f611045e9411efca9182b7d7d69e2e94465c6340
jyzib <jazibzaidi02@gmail.com>
2023-06-04 00:46:47 +0530

    Feat: Our mission section is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './component/navbar'
 import Home from './pages/home'
 import Login from './pages/login'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
